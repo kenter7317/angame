7590edb0eb32fe593fb55cbf436323c6e13577ee
kenter7317 <kenter7317@gmail.xom>
2026-05-06 16:36:41 +0900

    Draft for refactor delete useless resource

--- src/main/kotlin/per/kenter7317/item/ItemData.kt
+++ src/main/kotlin/per/kenter7317/item/ItemData.kt
@@ -1,7 +1,11 @@
 package per.kenter7317.item
+
+import java.awt.image.BufferedImage
+
 class ItemData (val itemName : String,
                 val itemID : Int,
-                val itemDescription : String
+                val itemDescription : String,
+                val itemImage: BufferedImage
 ){
 
 }
