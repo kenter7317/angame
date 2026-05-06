ce3549df34c464b021b7ab48a62f4c198899fc55
kenter7317 <kenter7317@gmail.xom>
2026-05-06 15:24:28 +0900

    Draft: draft for refactor refactor pre-interface implementation

--- src/main/kotlin/per/kenter7317/gui/SaveDataList.kt
+++ src/main/kotlin/per/kenter7317/gui/SaveDataList.kt
@@ -2,7 +2,10 @@ package per.kenter7317.gui
 
 import de.gurkenlabs.litiengine.graphics.Spritesheet
 import de.gurkenlabs.litiengine.gui.ImageComponentList
+import per.kenter7317.extension.SelectableItems
+import per.kenter7317.extension.util.RunnableString
 import java.awt.Image
+import java.util.function.IntConsumer
 
 class SaveDataList(
     x: Double,
@@ -13,6 +16,13 @@ class SaveDataList(
     columns: Int,
     images: MutableList<Image>?,
     background: Spritesheet?
-) : ImageComponentList(x, y, width, height, rows, columns, images, background) {
+) : SelectableItems, ImageComponentList(x, y, width, height, rows, columns, images, background) {
+
+    override var currentSelection: Int = 0
+    override var items: List<RunnableString> = emptyList()
+    override var selectionChangeConsumers: List<IntConsumer>? = null
+    override fun changeSelection(last: Int, current: Int) {
+
+    }
 
 }
