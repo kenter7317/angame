e1200ecad75bd7496d543cc90eca871cc937cede
dalmurii <mincraft20046@gmail.com>
2026-05-25 17:45:59 +0900

    update: made `RunnableString` open class to avoid `Item` extending it got crashed by final shit. 	wrote custom configurer for `Player` 	wrote type aliases for members on `AEntity`
    todo:	ship `Player`'s redundent default values to test.
    
     Changes to be committed:
    	- modified:   src/main/kotlin/per/kenter7317/data/entity/AEntity.kt
    	- modified:   src/main/kotlin/per/kenter7317/data/entity/player/Player.kt
    	- modified:   src/main/kotlin/per/kenter7317/data/item/Item.kt
    	- modified:   src/main/kotlin/per/kenter7317/extension/util/RunnableString.kt

--- src/main/kotlin/per/kenter7317/data/entity/AEntity.kt
+++ src/main/kotlin/per/kenter7317/data/entity/AEntity.kt
@@ -4,34 +4,36 @@ import java.util.UUID
 import de.gurkenlabs.litiengine.entities.Entity
 
 abstract class AEntity : Entity() {
+	/** any attributes */
+	typealias attr_t		= HashMap<EntityAttribute, Any?>
+	/** valid attributes */
+	typealias attr_val_t		= HashMap<EntityAttribute, Class<*>>
 
-    protected lateinit var attributes: HashMap<EntityAttribute, Any?>
+	protected lateinit var attributes		: attr_t
+	protected lateinit var possibleAttributes	: attr_val_t
 
-    protected lateinit var possibleAttributes: HashMap<EntityAttribute, Class<*>>
+	lateinit var uuid: UUID
+	protected fun updateAttributes(attributes: Map<EntityAttribute, Any>) {
+		attributes.forEach { (attribute, value) ->
+			if (hasAttribute(attribute) && isValidAttribute(attribute, value) && canHasAttribute(attribute)) {
+				this.attributes[attribute] = value
+			}
+		}
+	}
 
-    lateinit var uuid: UUID
+	fun getAttribute(attribute: EntityAttribute): Any {
+		return attributes[attribute] ?: throw IllegalArgumentException("Attribute not found")
+	}
 
-    protected fun updateAttributes(attributes: Map<EntityAttribute, Any>) {
-        attributes.forEach { (attribute, value) ->
-            if (hasAttribute(attribute) && isValidAttribute(attribute, value) && canHasAttribute(attribute)) {
-                this.attributes[attribute] = value
-            }
-        }
-    }
+	private fun hasAttribute(attribute: EntityAttribute): Boolean {
+		return attributes.containsKey(attribute)
+	}
 
-    fun getAttribute(attribute: EntityAttribute): Any {
-        return attributes[attribute] ?: throw IllegalArgumentException("Attribute not found")
-    }
+	private fun isValidAttribute(attribute: EntityAttribute, value: Any): Boolean {
+		return possibleAttributes.containsKey(attribute) && possibleAttributes[attribute] == value.javaClass
+	}
 
-    private fun hasAttribute(attribute: EntityAttribute): Boolean {
-        return attributes.containsKey(attribute)
-    }
-
-    private fun isValidAttribute(attribute: EntityAttribute, value: Any): Boolean {
-        return possibleAttributes.containsKey(attribute) && possibleAttributes[attribute] == value.javaClass
-    }
-
-    private fun canHasAttribute(attribute: EntityAttribute): Boolean {
-        return possibleAttributes.contains(attribute)
-    }
+	private fun canHasAttribute(attribute: EntityAttribute): Boolean {
+		return possibleAttributes.contains(attribute)
+	}
 }
